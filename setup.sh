740e457a6674464a82622c6704d25913c7f9b570
Dmitry Sukhikh <china@pozitive.biz>
2014-02-03 14:51:32 +0700

    Emacs 24

--- setup.sh
+++ setup.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# Simple setup.sh for configuring Ubuntu 13.10 EC2 instance
 # for headless setup.
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+# Install git, curl
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
@@ -39,9 +24,8 @@ fi
 if [ -d .emacs.d/ ]; then
 mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/coddes/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+git clone https://github.com/coddeys/dotfiles.git
+ln -sb dotfiles/.tmux.conf .
+ln -sb dotfiles/.zshrc .
+mv .emacs.d .emacs.d~
+ln -s dotfiles/.emacs.d .
